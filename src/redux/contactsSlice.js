90a77c5895cc661346da73c18ec291c76e26678c
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-23 20:04:10 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -10,6 +10,18 @@ const contactsSlice = createSlice({
   name: 'contacts',
   initialState: contactsInitialState,
   reducers: {
+    fetchingInProgress(state) {
+      state.isLoading = true;
+    },
+    fetchingSuccess(state, action) {
+      state.isLoading = false;
+      state.error = null;
+      state.items = action.payload;
+    },
+    fetchingError(state, action) {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
     addNewContact(state, action) {
       return [...state, { ...action.payload }];
     },
