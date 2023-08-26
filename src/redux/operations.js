fd7ff545a4e8df51191fb5e0ba490cee6935cfef
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-26 23:53:32 +0300

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -3,4 +3,14 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://mockapi.io/clone/64ea4377bf99bdcc8e6772c7';
 
-export const fetchContacts = createAsyncThunk('contacts');
+export const fetchContacts = createAsyncThunk(
+  'contacts',
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get('/tasks');
+      return response.data;
+    } catch (e) {
+      return thunkAPI.rejectWithValue(e.message);
+    }
+  }
+);
