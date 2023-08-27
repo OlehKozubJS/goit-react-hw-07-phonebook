2d3c1482b1eb5304bee88803f6b720d3d5636e47
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-27 18:01:17 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,2 +1,5 @@
+import { createSelector } from '@reduxjs/toolkit';
+import { statusFilters } from './constants';
+
 export const getContacts = state => state.contacts;
 export const getFilter = state => state.filter;
