2f5d7888d86f845e5471aae419b1dbe5abf5f89d
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-27 18:09:05 +0300

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -5,3 +5,35 @@ export const selectContacts = state => state.contacts.items;
 export const selectLoading = state => state.contacts.isLoading;
 export const selectError = state => state.contacts.error;
 export const getFilter = state => state.filter;
+
+export const selectVisibleTasks = createSelector(
+  [selectTasks, selectStatusFilter],
+  (tasks, statusFilter) => {
+    console.log('Calculating visible tasks');
+
+    switch (statusFilter) {
+      case statusFilters.active:
+        return tasks.filter(task => !task.completed);
+      case statusFilters.completed:
+        return tasks.filter(task => task.completed);
+      default:
+        return tasks;
+    }
+  }
+);
+
+export const selectTaskCount = createSelector([selectTasks], tasks => {
+  console.log('Calculating task count');
+
+  return tasks.reduce(
+    (count, task) => {
+      if (task.completed) {
+        count.completed += 1;
+      } else {
+        count.active += 1;
+      }
+      return count;
+    },
+    { active: 0, completed: 0 }
+  );
+});
