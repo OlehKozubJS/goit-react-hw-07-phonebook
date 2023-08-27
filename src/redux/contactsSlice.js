68515f0eb976818e15025e67f11910e959ecf009
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-27 17:57:55 +0300

    Update contactsSlice.js

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -38,7 +38,9 @@ const contactsSlice = createSlice({
     [deleteContact.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
-      state.items = state.filter(contact => contact.id !== action.payload);
+      state.items = state.items.filter(
+        contact => contact.id !== action.payload
+      );
     },
     [deleteContact.rejected]: handleRejected,
   },
