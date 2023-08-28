b7cca27ceb6fdf4d4cd86dfd3e2725eb13eab00c
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-28 21:11:02 +0300

    Update LoadAlert.jsx

--- src/components/LoadAlert.jsx
+++ src/components/LoadAlert.jsx
@@ -1,9 +1,9 @@
 import AlertStyles from './PhonebookCSS/Alert.module.css';
 
-export const LoadAlert = () => {
+export const LoadAlert = ({ children }) => {
   return (
     <div className={AlertStyles.alert}>
-      <span className={AlertStyles.alertText}></span>
+      <span className={AlertStyles.alertText}>{children}</span>
     </div>
   );
 };
