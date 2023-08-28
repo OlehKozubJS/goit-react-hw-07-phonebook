bfc2e251b4c51ef4ce373769d73a2ef652c6f313
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-28 20:29:37 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,14 @@ import { Filter } from './Filter';
 import PhonebookStyles from './PhonebookCSS/Pnonebook.module.css';
 
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchTasks());
+  }, [dispatch]);
+
   return (
     <div className={PhonebookStyles.phonebook}>
       <h1 className={PhonebookStyles.phonebookHeader}>Phonebook</h1>
