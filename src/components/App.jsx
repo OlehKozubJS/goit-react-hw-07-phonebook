76b9f0e1ae8d8ea3eb654c3d3461ba7a2b91323f
OlehKozubJS <oleg.kozub54@gmail.com>
2023-08-28 20:33:59 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,11 @@
+import { fetchContacts } from 'redux/operations';
 import { ContactForm } from './ContactForm';
 import { ContactList } from './ContactList';
 import { Filter } from './Filter';
 import PhonebookStyles from './PhonebookCSS/Pnonebook.module.css';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectIsLoading, selectError } from '../redux/selectors';
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -9,13 +13,14 @@ export const App = () => {
   const error = useSelector(selectError);
 
   useEffect(() => {
-    dispatch(fetchTasks());
+    dispatch(fetchContacts());
   }, [dispatch]);
 
   return (
     <div className={PhonebookStyles.phonebook}>
       <h1 className={PhonebookStyles.phonebookHeader}>Phonebook</h1>
       <ContactForm />
+      {isLoading && !error && <b>Request in progress...</b>}
       <h2 className={PhonebookStyles.contactsHeader}>Contacts</h2>
       <Filter className="filterInput" />
       <ContactList className="contactList" />
